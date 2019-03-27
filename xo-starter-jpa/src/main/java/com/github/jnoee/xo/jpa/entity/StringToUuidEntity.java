37bf5826a6e1d1163d44e62474e6f5055b90b83b
天朝红雨 <178668028@qq.com>
2019-03-27 12:50:23 +0800

    字符串转为实体时只赋值id

--- xo-starter-jpa/src/main/java/com/github/jnoee/xo/jpa/entity/StringToUuidEntity.java
+++ xo-starter-jpa/src/main/java/com/github/jnoee/xo/jpa/entity/StringToUuidEntity.java
@@ -1,15 +1,17 @@
 package com.github.jnoee.xo.jpa.entity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
-
-import com.github.jnoee.xo.jpa.dao.DaoUtils;
-import com.github.jnoee.xo.utils.StringUtils;
 
 /**
  * 字符串转换成UuidEntity转换器工厂。
  */
 public class StringToUuidEntity implements ConverterFactory<String, UuidEntity> {
+
+  private static Logger logger = LoggerFactory.getLogger(StringToUuidEntity.class);
+
   @Override
   public <T extends UuidEntity> Converter<String, T> getConverter(Class<T> targetType) {
     return new StringToUuidEntityConverter<>(targetType);
@@ -17,7 +19,7 @@ public class StringToUuidEntity implements ConverterFactory<String, UuidEntity> 
 
   /**
    * 字符串转换成UuidEntity转换器。
-   * 
+   *
    * @param <T> 转换类型
    */
   private class StringToUuidEntityConverter<T extends UuidEntity> implements Converter<String, T> {
@@ -25,7 +27,7 @@ public class StringToUuidEntity implements ConverterFactory<String, UuidEntity> 
 
     /**
      * 构造方法。
-     * 
+     *
      * @param toClass 转换目标类
      */
     public StringToUuidEntityConverter(Class<T> toClass) {
@@ -34,15 +36,19 @@ public class StringToUuidEntity implements ConverterFactory<String, UuidEntity> 
 
     /**
      * 将字符串值转换为UuidEntity对象。
-     * 
+     *
      * @param source 字符串值
      * @return 返回UuidEntity对象。
      */
     public T convert(String source) {
-      if (StringUtils.isBlank(source)) {
+      try {
+        T entity = toClass.newInstance();
+        entity.setId(source);
+        return entity;
+      } catch (ReflectiveOperationException e) {
+        logger.info(String.format("无法获得实例[%s]:%s", toClass, e.getMessage()), e);
         return null;
       }
-      return DaoUtils.getEntity(toClass, source.trim());
     }
   }
 }
